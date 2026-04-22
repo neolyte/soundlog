21b2a73ca04b8c3b16bc6ae98abfe191c012473c
Romain Brochard <romain@soundlining.com>
2026-04-22 09:00:50 +0200

    remove stdout_redirect in puma config for prod

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -9,31 +9,30 @@ tag app_name
 pidfile     "#{root_directory}/shared/tmp/pids/puma.pid"
 state_path  "#{root_directory}/shared/tmp/pids/puma.state"
 
-stdout_redirect(
-  "#{root_directory}/shared/log/puma_access.log",
-  "#{root_directory}/shared/log/puma_error.log",
-  true
-)
+# stdout_redirect(
+#   "#{root_directory}/shared/log/puma_access.log",
+#   "#{root_directory}/shared/log/puma_error.log",
+#   true
+# )
 
 threads_count_min = ENV.fetch("PUMA_THREADS_MIN", 3).to_i
 threads_count_max = ENV.fetch("PUMA_THREADS_MAX", 5).to_i
 threads threads_count_min, threads_count_max
 
-# workers ENV.fetch("PUMA_WORKERS", 2).to_i
-workers 0
+workers ENV.fetch("PUMA_WORKERS", 2).to_i
 
 bind "unix://#{root_directory}/shared/tmp/sockets/puma.sock"
 
-# preload_app!
+preload_app!
 
-# before_fork do
-#   if defined?(ActiveRecord::Base)
-#     ActiveRecord::Base.connection_pool.disconnect!
-#   end
-# end
+before_fork do
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection_pool.disconnect!
+  end
+end
 
-# before_worker_boot do
-#   if defined?(ActiveRecord::Base)
-#     ActiveRecord::Base.establish_connection
-#   end
-# end
+before_worker_boot do
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end
